432173918d1a1a490e7270e6cf96eff74fb1cadc
izaidetenutes <izaide.tenutes@id-cotton.com>
2023-04-27 11:15:16 -0400

    funcoes

---
+++ 04 - funcoes/aula.js
@@ -0,0 +1,15 @@
+
+
+function sayMyName(name){
+    console.log('Your name is'+ name);
+}
+
+sayMyName('Renan');
+sayMyName('Marcelo');
+
+function main(){
+    console.log('Programa principal');
+
+}
+
+main();
